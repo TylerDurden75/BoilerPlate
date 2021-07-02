b71df03ae905fcfd77044210339d58f932ec4773
TylerDurden75 <raytibo.j@gmail.com>
2021-07-02 16:44:26 +0200

    starting animations

--- app/index.js
+++ app/index.js
@@ -1,1 +1,77 @@
-console.log("Hello World !");
+import About from "pages/About";
+import Home from "pages/Home";
+import Collections from "pages/Collections";
+import Detail from "pages/Detail";
+
+import each from "lodash/each";
+
+class App {
+  constructor() {
+    this.createContent();
+    this.createPages();
+
+    this.addLinkListeners();
+  }
+
+  createContent() {
+    this.content = document.querySelector(".content");
+    this.template = this.content.getAttribute("data-template");
+  }
+
+  createPages() {
+    this.pages = {
+      about: new About(),
+      home: new Home(),
+      collections: new Collections(),
+      detail: new Detail(),
+    };
+
+    this.page = this.pages[this.template];
+    this.page.create();
+
+    this.page.show();
+  }
+
+  async onChange(url) {
+    await this.page.hide();
+
+    const request = await window.fetch(url);
+
+    if (request.status === 200) {
+      const html = await request.text();
+      const div = document.createElement("div");
+
+      div.innerHTML = html;
+
+      const divContent = div.querySelector(".content");
+
+      this.template = divContent.getAttribute("data-template");
+
+      this.content.setAttribute("data-template", this.template);
+      this.content.innerHTML = divContent.innerHTML;
+
+      this.page = this.pages[this.template];
+      this.page.create();
+      this.page.show();
+
+      this.addLinkListeners();
+    } else {
+      console.log("Error");
+    }
+  }
+
+  addLinkListeners() {
+    const links = document.querySelectorAll("a");
+
+    each(links, (link) => {
+      link.onclick = (event) => {
+        event.preventDefault();
+        const { href } = link;
+
+        this.onChange(href);
+      };
+    });
+  }
+}
+
+new App();
